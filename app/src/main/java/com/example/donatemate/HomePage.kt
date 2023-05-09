cecc5615e685319d2d52ff37be05e620596c84f2
Chalani Rajapaksha <chalani.rajapaksha@elegantmedia.lk>
2023-05-10 01:11:28 +0530

    CRUD Completed

--- app/src/main/java/com/example/donatemate/HomePage.kt
+++ app/src/main/java/com/example/donatemate/HomePage.kt
@@ -1,11 +1,41 @@
 package com.example.donatemate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+
+        val button1: LinearLayout = findViewById(R.id.askdonat)
+        button1.setOnClickListener {
+            val intent = Intent(this@HomePage, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //payment money
+        val button2: LinearLayout = findViewById(R.id.payment)
+        button2.setOnClickListener {
+            val intent = Intent(this@HomePage, Create_Payment::class.java)
+            startActivity(intent)
+        }
+
+        //memori
+        val button3: LinearLayout = findViewById(R.id.memoria)
+        button3.setOnClickListener {
+            val intent = Intent(this@HomePage, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //profile
+        val button4: LinearLayout = findViewById(R.id.profil)
+        button4.setOnClickListener {
+            val intent = Intent(this@HomePage, Userprofile::class.java)
+            startActivity(intent)
+        }
     }
 }
